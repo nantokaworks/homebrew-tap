5debac8cb6ebb282855c773e7ecf4df3c8c50ac2
ICH <ichi0g0y@gmail.com>
2026-06-30 22:07:55 +0900

    Update robco formula to 0.1.8

--- Formula/robco.rb
+++ Formula/robco.rb
@@ -1,7 +1,7 @@
 class Robco < Formula
   desc "Repo-oriented terminal cockpit for supervising AI coding agents"
   homepage "https://github.com/nantokaworks/robco"
-  version "0.1.7"
+  version "0.1.8"
 
   livecheck do
     url "https://github.com/nantokaworks/robco-releases/releases.atom"
@@ -10,23 +10,23 @@ class Robco < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/nantokaworks/robco-releases/releases/download/v0.1.7/robco-0.1.7-aarch64-apple-darwin.tar.gz"
-      sha256 "76fb5f18e6e7c6cd328035348f1c7a54038713bb7e810ed94b5c4a5b3100bef9"
+      url "https://github.com/nantokaworks/robco-releases/releases/download/v0.1.8/robco-0.1.8-aarch64-apple-darwin.tar.gz"
+      sha256 "30c0da64af03c0baf48681e5075863324b6c7e7f79aea80bbdae3aebcf0f2b1b"
     end
     on_intel do
-      url "https://github.com/nantokaworks/robco-releases/releases/download/v0.1.7/robco-0.1.7-x86_64-apple-darwin.tar.gz"
-      sha256 "c4bb60ca4c83150493ad747a93c94c3a191e7046e190c026c7790088ee17fb30"
+      url "https://github.com/nantokaworks/robco-releases/releases/download/v0.1.8/robco-0.1.8-x86_64-apple-darwin.tar.gz"
+      sha256 "64adcedf21f8630273d724f3da5af9f439266b34d185266a7fdda4deda12e66b"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/nantokaworks/robco-releases/releases/download/v0.1.7/robco-0.1.7-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "d91d3c520a56e426e2364c2da3d0393555bd17f0c1a1295eeb060dde7d241db0"
+      url "https://github.com/nantokaworks/robco-releases/releases/download/v0.1.8/robco-0.1.8-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "a9bc93735883c35865c8632bb63a5acd01931e517562dde1cbc6aef57f63a7e2"
     end
     on_intel do
-      url "https://github.com/nantokaworks/robco-releases/releases/download/v0.1.7/robco-0.1.7-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "0e085c161b3f9c826ec8eb36599a154ed20df82efa5fd62d6411d98bcbefc212"
+      url "https://github.com/nantokaworks/robco-releases/releases/download/v0.1.8/robco-0.1.8-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "c9c2aeeabc96d50bb58101c1b783ca5efdea8f47a04d11797b347031acb6312c"
     end
   end
 
